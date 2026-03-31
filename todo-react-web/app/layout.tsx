5f295fed2c0fad6f7a9455fff5775369f9a9d4a0
Giovanni S Araki <giovanni.saraoaraki@gmail.com>
2026-03-30 22:13:25 -0300

    feat: SpeedInsight

--- todo-react-web/app/layout.tsx
+++ todo-react-web/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import ApolloClientProvider from "@/lib/ApolloProvider";
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import "./globals.css";
 
@@ -33,6 +34,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <ApolloClientProvider>{children}</ApolloClientProvider>
          <Analytics />
+         <SpeedInsights/>
       </body>
     </html>
   );
