cda0ccaeaf695271e9b5aec6080a63ce2f0e16d2
Giovanni S Araki <giovanni.saraoaraki@gmail.com>
2026-09-08 19:52:24 -0300

    fix: force webpack for next-pwa compatibility

--- todo-react-web/next.config.ts
+++ todo-react-web/next.config.ts
@@ -1,17 +1,15 @@
-import withPWA from 'next-pwa';
+import withPWA from "next-pwa";
 
 const pwaConfig = withPWA({
-  dest: 'public',
+  dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development', // ← desativa em dev
+  disable: process.env.NODE_ENV === "development", // ← desativa em dev
 });
 
 const nextConfig = {
- experimental: {
-    turbo: undefined,
-  },
-    serverExternalPackages: ["jspdf"]
+  turbopack: {},
+  serverExternalPackages: ["jspdf"],
 };
 
 export default pwaConfig(nextConfig);
